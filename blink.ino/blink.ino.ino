b6abb627863446d1bf35a49d95c08ee646491144
AtharvPatil8147 <athsanpat@gmail.com>
2024-05-09 18:51:01 +0530

    led trail

--- blink.ino/blink.ino.ino
+++ blink.ino/blink.ino.ino
@@ -1,9 +1,22 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(4,OUTPUT);
+  delay(50);
+  digitalWrite(4,LOW);
+  delay(50);
   digitalWrite(2,OUTPUT);
+  delay(50);
+  digitalWrite(2,LOW);
+  delay(50);
+  digitalWrite(3,OUTPUT);
+  delay(50);
+  digitalWrite(3,LOW);
+  delay(100);
 }
